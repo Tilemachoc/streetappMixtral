3024ba242604957eaa471f51e54d2694309f7b02
Tilemachos Chatzipapas <138583191+Tilemachoc@users.noreply.github.com>
2024-04-02 17:40:20 +0300

    Update settings.py

--- web_proj/web_proj/settings.py
+++ web_proj/web_proj/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = django_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
